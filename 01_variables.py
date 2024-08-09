74fe1172f12daf3fac2676c7a21a91d520572ebe
ishahulahmed <shahulahmed1717@gmail.com>
2024-08-09 16:23:11 +0530

    python-devops

--- 01_variables.py
+++ 01_variables.py
@@ -5,7 +5,23 @@ this is a multi-line comment
 """
 
 # Define a variable
-a = 42
+a = 42 # This is a integer variable
 
 # Print the value that is stored inside the variable
 print(a)
+
+# Float Value
+b = 42.345
+print(b)
+
+# Boolean
+c = True
+
+# Strings
+d = 'This is a string'
+e = "This is also a string"
+f = """This is a multi-line sring"""
+
+# Today's weather is nice
+d = 'Today's weather is nice'
+print(d)
