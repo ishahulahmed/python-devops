04f0eb135fd1ad408b817f99ee73dadfcdf2bfa0
ishahulahmed <shahulahmed1717@gmail.com>
2024-08-11 00:14:40 +0530

    python-devops

---
+++ 02_strings.py
@@ -0,0 +1,34 @@
+sample_str = "This-is-a-sample-string"
+print(sample_str)
+
+# How to access individual characters from a string
+print(sample_str[8])
+
+# Slicing
+sub_str = sample_str[2:7]
+print(sub_str)
+
+sub_str = sample_str[:]
+print(sub_str)
+
+sub_str = sample_str[1:]
+print(sub_str)
+
+sub_str = sample_str[:5]
+print(sub_str)
+
+sub_str = sample_str[::2]
+print(sub_str)
+
+# Reverse a string
+sub_str = sample_str[::-1]
+print(sub_str)
+
+# lenght of a string
+len_str = len(sample_str)
+print("Lenght of a string:", len_str)
+
+# Method
+sample_str = "hello"
+print(sample_str.capitalize())
+
