393b8c05310afeba444f0ebe244902e33135da93
ishahulahmed <shahulahmed1717@gmail.com>
2024-08-14 18:57:48 +0530

    python-devops

--- 07_type_conversion.py
+++ 07_type_conversion.py
@@ -6,12 +6,15 @@ sample_str_tuple = tuple(sample_str)
 print(sample_str_tuple)
 
 # Accept input from a user
-user_input = input("Enter a number:")
+user_input = input("Enter a number: ")
 print(user_input, type(user_input))
 
-# Enter a number:33
+# Enter a number: 33
 # 33 <class 'str'>
 
-# add_10 = user_input + 10
+# add_10 = int(user_input) + 10
 # print(add_10)
-# TypeError: can only concatenate str (not "int") to str
+# ValueError: invalid literal for int() with base 10: '10 100 200 300 500 700'
+
+split_input = "10 100 200 500 700".split(" ")
+print(split_input)
